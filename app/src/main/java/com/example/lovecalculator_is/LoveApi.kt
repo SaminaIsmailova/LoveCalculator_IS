caf61bb1213d27698b668a726a5b8860951a3035
SaminaIsmailova <samina.ismailova.06@gmail.com>
2024-02-03 15:06:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lovecalculator_is/LoveApi.kt
@@ -0,0 +1,14 @@
+package com.example.lovecalculator_is
+
+import android.telecom.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface LoveApi {
+    @GET("getPercentage")
+    fun getLove(
+        @Query("sname") secondName: String, @Query("fname") firstName: String,
+        @Header("X-RapidAPI-Key") key: String = "62046210eamshb1039cc3a834d5ep160059jsn329f9fbbe521",
+        @Header("X-RapidAPI-Host") host: String = "love-calculator.p.rapidapi.com"
+    ): Call<LoveModel>}
---
+++ app/src/main/java/com/example/lovecalculator_is/LoveModel.kt
@@ -0,0 +1,9 @@
+package com.example.lovecalculator_is
+
+data class LoveModel(
+
+    val fname: String,
+    val sname: String,
+    val percentage: String,
+    val result: String,
+    )
